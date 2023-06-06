008cda9c113a18dee1a2e42eca8fd31a77eab1e8
harish242 <120125264+harish242@users.noreply.github.com>
2023-06-06 20:34:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,102 @@
-import React from 'react'
+import React, { useState } from 'react';
 import '../styles/App.css';
-import {useState} from 'react';
-import {signUpFormValidation} from '../utils/validation'
-
-
-
+import { signUpFormValidation } from '../utils/validation';
 
 const App = () => {
-  const [formDetail,setDetails]=useState({name:"",email:"",password:"",consent:false})
-  const[errors,setErrors]=useState({})
- const handleNameChange=(event)=>{
-     const field=event.target.id
-     const value=event.target.value
-     setDetails({...formDetail,[field]:value})
- }
- const handleSubmit=(event)=>{
-  // event.preventDefault()
-  const validationErrors=signUpFormValidation(formDetail)
-  if(validationErrors){
-    setErrors(validationErrors)
-  }
- }
-  
-  return (
-   <>
-   <form onChange={handleNameChange} onSubmit={handleSubmit}>
-   <label htmlFor='name'>Name:</label>
-   <input type='text' id='name' value={formDetail.name}/>
-   {errors.name && <span className="error">{errors.name}</span>}
-   <label htmlFor='email'>Email:</label>
-   <input type='email' id='email'/>
-   {errors.email && <span className="error">{errors.email}</span>}
-   <label htmlFor='password'>Password:</label>
-   <input type='password' id='password' value={errors.password}/>
-   {errors.password && <span className="error">{errors.password}</span>}
-   <label htmlFor='consent'>Checkbox:</label>
-   <input type='checkbox' id='consent' />
-   <button type='submit'>Signup</button>
-   </form>
-   
-   </>
-  )
-}
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [consent, setConsent] = useState(false);
+  const [errors, setErrors] = useState({});
 
+  const handleInputChange = (e) => {
+    const { id, value, checked, type } = e.target;
+
+    if (type === 'checkbox') {
+      setConsent(checked);
+    } else {
+      if (id === 'name') setName(value);
+      else if (id === 'email') setEmail(value);
+      else if (id === 'password') setPassword(value);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const fields = {
+      name: name.trim(),
+      email: email.trim(),
+      password: password.trim(),
+    };
+
+    const validationErrors = signUpFormValidation(fields);
+
+    if (validationErrors) {
+      setErrors(validationErrors);
+    } else {
+      // Form submission logic goes here
+      // Reset the form and errors
+      setName('');
+      setEmail('');
+      setPassword('');
+      setConsent(false);
+      setErrors({});
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={handleInputChange}
+          />
+          {errors.name && <div className="error">{errors.name}</div>}
+        </div>
+
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleInputChange}
+          />
+          {errors.email && <div className="error">{errors.email}</div>}
+        </div>
+
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handleInputChange}
+          />
+          {errors.password && <div className="error">{errors.password}</div>}
+        </div>
+
+        <div>
+          <label htmlFor="consent">
+            <input
+              type="checkbox"
+              id="consent"
+              checked={consent}
+              onChange={handleInputChange}
+            />
+            I agree to the terms and conditions
+          </label>
+        </div>
+
+        <button type="submit">Signup</button>
+      </form>
+    </div>
+  );
+};
 
 export default App;
